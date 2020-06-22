604e29161bd3814fa91c61ae24e5d03ad46e5741
Alessandro Solbiati <lessandro@fb.com>
2020-06-22 02:26:53 +0100

    [ecto] added player

---
+++ lib/gateway89/player.ex
@@ -0,0 +1,18 @@
+defmodule Gateway89.Player do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "players" do
+    field :barions, :integer
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(player, attrs) do
+    player
+    |> cast(attrs, [:name, :barions])
+    |> validate_required([:name, :barions])
+  end
+end
