cc59666cf78bad333977c521ffa96d7770f650c7
Alessandro Solbiati <lessandro@fb.com>
2020-06-22 04:14:20 +0100

    [intro] changed wording

--- lib/gateway89_web/controllers/home_controller.ex
+++ lib/gateway89_web/controllers/home_controller.ex
@@ -8,7 +8,7 @@ defmodule Gateway89Web.HomeController do
 
   def index(conn, _params) do
     intro_message =
-      "We need your help. This is Gateway89, the only communication channel left after humanity proved that the universe is a simulation, we are speaking to you from the other side. Right now we need you to keep Gateway89 open to give you further instructions. To send messages you will need to generate Barions, the elementary component of your simulation. Barions are extremely hard to deal with, the simulation is designed to annihilate them to prevent cross-simulation channels like Gateway89. You will need to be skillfull to handle the large complexity of the simulation, to start playing access http://www.gateway89.com/start"
+      "We need your help. This is Gateway89, the only communication channel left after humanity proved that the universe is a simulation: we are speaking to you from the other side and we need your help to keep Gateway89 open. To send messages you will need to generate Barions, the elementary component of your simulation. Barions are extremely hard to deal with, the simulation is designed to annihilate them to prevent cross-simulation channels like Gateway89. You will need to be skillfull to handle the large complexity of the simulation, to start playing access http://www.gateway89.com/start"
 
     render(conn, "message.json", message: intro_message)
   end
